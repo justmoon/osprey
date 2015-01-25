e0c10e7ab27596e880124edbbe7995cd86b13cef
Stefan Thomas <justmoon@members.fsf.org>
2015-01-25 09:53:20 -0800

    Update JS.

--- dist/error-default-settings.js
+++ dist/error-default-settings.js
@@ -1,22 +1,22 @@
 (function() {
   module.exports = {
     InvalidAcceptTypeError: function(err, req, res) {
-      return res.send(406);
+      return res.status(406);
     },
     InvalidContentTypeError: function(err, req, res) {
-      return res.send(415);
+      return res.status(415);
     },
     InvalidUriParameterError: function(err, req, res) {
-      return res.send(400);
+      return res.status(400);
     },
     InvalidFormParameterError: function(err, req, res) {
-      return res.send(400);
+      return res.status(400);
     },
     InvalidQueryParameterError: function(err, req, res) {
-      return res.send(400);
+      return res.status(400);
     },
     InvalidHeaderError: function(err, req, res) {
-      return res.send(400);
+      return res.status(400);
     },
     InvalidBodyError: function(err, req, res) {
       res.status(400);
@@ -29,7 +29,13 @@
       });
     },
     Error: function(err, req, res) {
-      return res.send(500);
+      res.status(500);
+      return res.json({
+        error: {
+          id: "Internal Error",
+          message: err.message
+        }
+      });
     }
   };
 
